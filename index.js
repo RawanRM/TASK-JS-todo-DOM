f8e3d38e10ffc04fb3d09164b1c2127e1a0567bc
Good Lookin <r_a_msmlm@outlook.com>
2022-04-09 17:40:29 +0300

    PART1 AND PART 2 DONE in index.js

--- index.js
+++ index.js
@@ -1,40 +1,72 @@
 // You do not need to change the constants
+//SELECT CAT LIST IDS
 const CATEGORY_SELECTOR = "categories-list";
 const CATEGORY_FILTER = "categories-list-filter";
 
+//MY BUTTONS IDS
+let myListButton = document.getElementById("list-button");
+let myCatButton = document.getElementById("cat-button");
+
+
+//tasks is an ARRAY OF OBJECTS!!!
+//OBJ nature of tasks in task: 1.the id 2.the title 3.the category 4.the state
 let tasks = [];
 let categories = [];
+//objects for the tasks
+const taskobj ={};
 
 // REMOVE ME: SAMPLE FILLING
-tasks = [
-  { id: 0, title: "Game of thrones", category: "Movies", done: false },
-  { id: 1, title: "Toy Story 4", category: "Movies", done: false },
-];
 
 categories = ["Movies", "Groceries"];
 // SAMPLE
 renderCategories(categories, CATEGORY_SELECTOR);
 renderCategories(categories, CATEGORY_FILTER);
-renderTasks(tasks, "tasks-list");
+//basically; take whats in tasks, display it in tasks-list section
+//of the page.
+//SHOW ALL TASKS in tasks, OF tasks-list ON PAGE
+
+
+//RAWAN PERSONAL TESTING OF CODE
+//test the interactive button.
+
+//const for creating task objects faster
+function tskObj(id, title, category) {
+  this.id = id;
+  this.title = title;
+  this.category = category;
+  // this.done = done;
+}
 
 function taskChecked(taskId, checked) {
   // implement the delete task.
   // You are given the task id
+
   console.log(`${checked ? "" : "UN"}CHECKED TASK`, taskId);
 }
 
-function addTask() {
+//making button clickable and adds to list
+myListButton.addEventListener("click",function addTask() {
+  //gets id
   const selectedCategory = getSelectedCategoryById(CATEGORY_SELECTOR);
-  const taskTitle = getNewTaskText();
+  const taskTitles = getNewTaskText();
   // continue the code here
-  alert(`Category: ${selectedCategory} | Task: ${taskTitle}`);
-}
+  tasks.push(new tskObj((tasks.length-1)+1,taskTitles,selectedCategory));
+  renderTasks(tasks, "taskslist");
 
-function addCategory() {
+  alert(`Category: ${selectedCategory} | Task: ${taskTitles}`);
+});
+
+//making button clickable and adds to list
+myCatButton.addEventListener("click",function addCategory() {
   const newCategory = getNewCategoryText();
   // continue the code here
+  categories.push(newCategory);
+  renderCategories(categories, CATEGORY_SELECTOR);
+
   alert(`New category was added: ${newCategory}`);
-}
+});
+
+
 
 function filterTasks() {
   const selectedCategory = getSelectedCategoryById(CATEGORY_FILTER);
